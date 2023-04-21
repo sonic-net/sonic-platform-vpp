5a897e3ae7db6fee50c597d506969fe4d8503c44
nikunjdh <40279495+nikunjdh@users.noreply.github.com>
2023-04-21 14:05:08 -0700

    Test for IPV6 and NO_LINUX_NL
    Updated automation test to add test cases for
    - IPV6 Addresses 
    - NO_LINUX_NL support 
    Signed-off-by: nsdesai@gmail.com

--- tests/validate-sonic-vpp-docker-routing.py
+++ tests/validate-sonic-vpp-docker-routing.py
@@ -23,40 +23,57 @@ from jinja2 import Environment, FileSystemLoader
 def getIppoolAndCidrMask(ipPoolCidr):
     separator = "/"
     ippool = ipPoolCidr.split(separator)
+
+    if len(ippool) != 2:
+        print(f'Invalid IP Pool - "{ipPoolCidr}" is specified')
+        sys.exit(1)
+
     return ippool[0], ippool[1]
 
 def checkPosInteger(num):
     if not num.isdigit():
         print(f'Invalid IPPool has been provided')
-        quit()
+        sys.exit(1)
 
 def checkIp(ip):
     try:
         ipaddress.ip_address(ip)
     except ValueError:
         print(f'Invalid IP address specified - {ip}')
-        quit()
+        sys.exit(1)
 
 def checkAction(action):
 
     if  action != "build" and action != "tear" and action != "build,tear":
         print("Invalid action specified")
-        quit()
+        sys.exit(1)
   
 
 def checkTopology(topology):
     if not (topology == "singleHop" or topology == "multiHop"):
         print("Invalid topology specified")
-        quit()
+        sys.exit(1)
 
 def checkRouting(routing):
     if not (routing == "static" or routing == "bgp"):
         print("Invalid routing protocol specified")
-        quit()
+        sys.exit(1)
 
-def getSubnet(ippool):
+def checkIpType(ip, iptype):
 
-    separator = "."
+    if iptype == "ipv4":
+
+        if not type(ipaddress.ip_address(ip)) is ipaddress.IPv4Address:
+            print(f'Invalid IPV4 address - "{ip}" is specified')
+            sys.exit(1)
+
+    elif iptype == "ipv6":
+         if not type(ipaddress.ip_address(ip)) is ipaddress.IPv6Address:
+            print(f'Invalid IPV6 address - "{ip}" is specified')
+            sys.exit(1)        
+
+
+def getSubnet(ippool, separator):
 
     subnetArr = ippool.split(separator)
 
@@ -70,11 +87,16 @@ def checkVerboselog(verboselog):
     for i in verboselog:
         if i != 'v' and verboselog.count(i) != len(verboselog):
             print("Invalid verbose argument passed. Please pass like - 'v' or 'vv' or 'vvv' or 'vvvv' or 'vvvvvv")
-            quit()
+            sys.exit(1)
 
 
-def createInventoryFromTemplate(subnet, username, password, host):
+def checkNoLinuxNl(noLinuxNl):
 
+    if noLinuxNl != "yes" and noLinuxNl != "no":
+        print("Invalid 'noLinuxNl' argument specified. Enter 'yes' or 'no'")
+        sys.exit(1)
+
+def createInventoryFromTemplate(subnet, username, password, host, subnetV6, noLinuxNl):
     environment = Environment(loader=FileSystemLoader("."))
 
     #inventory.j2
@@ -82,17 +104,27 @@ def createInventoryFromTemplate(subnet, username, password, host):
 
     #new inventory file with values filled at templatized variables
     filename = "inventory.yaml"
+
+    noLinuxNlArg = ""
+
+    if noLinuxNl == "yes":
+        noLinuxNlArg = "y"
+    elif noLinuxNl == "no":
+        noLinuxNlArg = "n"
+
     content = template.render(
         subnet = subnet,
         username = username,
         password = password,
-        host = host
+        host = host,
+        ipv6_subnet = subnetV6,
+        no_linux_nl = noLinuxNlArg
     )
     with open(filename, mode="w", encoding="utf-8") as message:
         message.write(content)
         print(f"... wrote {filename}")
 
-def build(ansiblePlaybook, topology, verboselogArg):
+def execute(ansiblePlaybook, topology, verboselogArg):
 
     cmd = ['ansible-playbook', '-i', 'inventory.yaml',  ansiblePlaybook, '--extra-vars', '@inventory.yaml', '--tags', topology,  verboselogArg]
     print(cmd)
@@ -106,42 +138,16 @@ def build(ansiblePlaybook, topology, verboselogArg):
         if  re.search('failed=', loglineDecoded) != None and re.search('unreachable=', loglineDecoded) != None:
             if None == re.search('failed=0', loglineDecoded):
                 print(f'failed to execute {loglineDecoded.split().pop(0)} ansible {topology} task')
-                quit()
+                sys.exit(1)
             if None == re.search('unreachable=0', loglineDecoded):
                 print(f'failed to execute {loglineDecoded.split().pop(0)} ansible {topology} task, VM is unreachable')
-                quit()
+                sys.exit(1)
 
         if None != re.search('ok=0', loglineDecoded):
             print(f'unable to execute {loglineDecoded.split().pop(0)} ansible {topology} task')
-            quit()
+            sys.exit(1)
 
-    print("SUCCESSFUL in validating SONiC routing...\n")
    
-
-def tear(ansiblePlaybook, topology, verboselogArg):
-
-    cmd = ['ansible-playbook', '-i', 'inventory.yaml', ansiblePlaybook, '--tags', topology, verboselogArg]
-
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    for l in p.stdout.readlines():
-        loglineDecoded = l.decode()
-        print(loglineDecoded)
-
-        if  re.search('failed=', loglineDecoded) != None and re.search('unreachable=', loglineDecoded) != None:
-            if None == re.search('failed=0', loglineDecoded):
-                print(f'failed to execute {loglineDecoded.split().pop(0)} ansible {topology} task')
-                quit()
-            if None == re.search('unreachable=0', loglineDecoded):
-                print(f'failed to execute {loglineDecoded.split().pop(0)} ansible {topology} task, VM is unreachable')
-                quit()
-
-        if None != re.search('ok=0', loglineDecoded):
-            print(f'unable to execute {loglineDecoded.split().pop(0)} ansible {topology} task')
-            quit()  
-
-    print("SUCCESSFUL in tearing down...\n")    
-
 
 def runSonic(action, topology, routing, verboselogArg):
 
@@ -151,11 +157,14 @@ def runSonic(action, topology, routing, verboselogArg):
         print("ansiblePlaybook : ", ansiblePlaybook, "\n")
         if topology == "multiHop":
            topology = topology + ',' + routing
-        build(ansiblePlaybook, topology, verboselogArg)
+        execute(ansiblePlaybook, topology, verboselogArg)
+        print("SUCCESSFUL in validating SONiC routing...\n")
+
     elif action == "tear":
         ansiblePlaybook = "sonic-sw-dp-bring-down.yaml"
         print("ansiblePlaybook : ", ansiblePlaybook, "\n")
-        tear(ansiblePlaybook, topology, verboselogArg)
+        execute(ansiblePlaybook, topology, verboselogArg)
+        print("SUCCESSFUL in tearing down...\n")    
 
 
 
@@ -166,10 +175,12 @@ parser.add_argument("--action", type=str, help="enter action -  {'build' | 'tear
 parser.add_argument("--topology", type=str, help="enter topology - {'singleHop' | 'multiHop'}", nargs='?', default="multiHop")
 parser.add_argument("--routing", type=str, help="enter routing protocol - {'static' | 'bgp'}", nargs='?', default="bgp")
 parser.add_argument("--IPPool", type=str, help="enter IPPool - example : '172.16.0.0/16'", nargs='?', default="172.16.0.0/16") 
+parser.add_argument("--IPV6Pool", type=str, help="enter IPV6Pool - example : '2001:0db8::/64'", nargs='?', default="2001:0db8::/64") 
 parser.add_argument("--host", type=str, help="enter host - example : '10.64.98.181'", nargs='?',required=True )   
 parser.add_argument("--username", type=str, help="enter SSH username for host", nargs='?',required=True ) 
 parser.add_argument("--password", type=str, help="enter SSH user password for host", nargs='?',required=True )                       
 parser.add_argument("--verboselog", type=str, help="enter verbose logging - example : 'v or vv or vvv etc'", nargs='?', default="v")
+parser.add_argument("--noLinuxNl", type=str, help="enter noLinuxNl - example : 'yes' or 'no'", nargs='?', default="yes")
 
 
 args = parser.parse_args()
@@ -179,10 +190,12 @@ action = args.action
 topology = args.topology
 routing = args.routing
 ippoolCidr = args.IPPool
+ipv6poolCidr = args.IPV6Pool
 host = args.host
 username = args.username
 password = args.password
 verboselog = args.verboselog
+noLinuxNl = args.noLinuxNl
 
 
 #check action
@@ -200,11 +213,29 @@ ipPool, cidrMask = getIppoolAndCidrMask(ippoolCidr)
 #validate ippool
 checkIp(ipPool)
 
+#check if ipv4
+checkIpType(ipPool, "ipv4")
+
 #validate cidrMask
 checkPosInteger(cidrMask)
 
 #extract subnet
-subnet =  getSubnet(ipPool)
+subnet =  getSubnet(ipPool, ".")
+
+#get ipv6pool
+ipV6Pool, cidrV6Mask = getIppoolAndCidrMask(ipv6poolCidr)
+
+#validate ipv6pool
+checkIp(ipV6Pool)
+
+#validate ipv6
+checkIpType(ipV6Pool, "ipv6")
+
+#validate cidrMask
+checkPosInteger(cidrV6Mask)
+
+#extract subnet
+subnetV6 =  getSubnet(ipV6Pool, ":")
 
 #validate host
 checkIp(host)
@@ -212,13 +243,16 @@ checkIp(host)
 #validate verbose argument
 checkVerboselog(verboselog)
 
+#validate noLinuxNl
+checkNoLinuxNl(noLinuxNl)
+
 print('Arguments passed:\n')
 print('_________________________\n')
-print(f'action - {action}\ntopology - {topology}\nrouting - {routing}\nippool - {ippoolCidr}\nusername - {username}\npassword - {password}\nhost - {host}\nverbose - {verboselog}')
+print(f'action - {action}\ntopology - {topology}\nrouting - {routing}\nippool - {ippoolCidr}\nipv6pool - {ipv6poolCidr}\nusername - {username}\npassword - {password}\nhost - {host}\nverbose - {verboselog}\nnoLinuxNl - {noLinuxNl}')
 print('_________________________\n')
 
 #create inventory yaml file
-createInventoryFromTemplate(subnet, username, password, host)
+createInventoryFromTemplate(subnet, username, password, host, subnetV6, noLinuxNl)
 
 #run python script
 if action == "build,tear":
@@ -230,4 +264,4 @@ else:
 
 print("OPERATION COMPLETE...\n")    
 
-
+sys.exit(0)
