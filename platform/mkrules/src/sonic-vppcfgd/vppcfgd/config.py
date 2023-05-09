194b006b4201c0ee7823491180bea2a1a139a0e5
Shashidhar Patil <shashidhar.patil@gmail.com>
2023-05-09 17:58:29 +0530

    Issue #30: Support configuration of VPP bootup parameters
    Updated some files with copyright section as required.
    
    Signed-off-by: Shashidhar Patil <shashidhar.patil@gmail.com>

--- platform/mkrules/src/sonic-vppcfgd/vppcfgd/config.py
+++ platform/mkrules/src/sonic-vppcfgd/vppcfgd/config.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 from .vars import g_dev_debug, VPP_CONFIG_JSON_PATH, VPP_CFG_STARTUP_PATH, VPP_BOOTUP_CFG_PATH, INDENT
 from .log import dev_debug, log_notice
