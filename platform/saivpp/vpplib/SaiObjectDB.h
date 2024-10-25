acda29239407d05978fa2d867ae727a4a9a184fa
yue-fred-gao <132678244+yue-fred-gao@users.noreply.github.com>
2024-10-25 13:55:16 -0400

    Fix issue #115: NexthopGroup member change is not supported (#120)
    * Fix issue #115: NexthopGroup member change is not supported

--- platform/saivpp/vpplib/SaiObjectDB.h
+++ platform/saivpp/vpplib/SaiObjectDB.h
@@ -31,6 +31,14 @@ namespace saivpp
     */
     class SwitchStateBase;
     class SaiDBObject;
+
+    typedef struct _SaiChildRelation
+    {
+        sai_object_type_t parent_type;
+        // the attribute id in child object pointing to Parent object
+        sai_attr_id_t child_link_attr;
+        sai_attr_value_type_t child_link_attr_type;
+    } SaiChildRelation;
     
     /*
       SaiObject is the base class for all objects in the SaiObjectDB.
@@ -43,10 +51,16 @@ namespace saivpp
 
         virtual ~SaiObject() = default;
 
-        virtual sai_status_t get_attr(_Out_ sai_attribute_t &attr) const = 0;
+        virtual sai_status_t get_attr(_Inout_ sai_attribute_t &attr) const = 0;
 
-        // Get the manditory attribute of the object. If not found, log an error and return SAI_STATUS_FAILURE
-        virtual sai_status_t get_mandatory_attr(_Out_ sai_attribute_t &attr) const = 0;
+        /**
+         * @brief Retrieves the value of a specific attribute of the SAI object. If the attribute is not found, log an error and 
+         *        return SAI_STATUS_FAILURE.
+         * 
+         * @param attr [out] A reference to the sai_attribute_t structure to store the attribute value.
+         * @return sai_status_t The status of the attribute retrieval operation.
+         */
+        sai_status_t get_mandatory_attr(_Inout_ sai_attribute_t &attr) const;
 
         const std::string& get_id() const { return m_id; }
 
@@ -95,16 +109,7 @@ namespace saivpp
          * @param attr [out] A reference to the sai_attribute_t structure to store the attribute value.
          * @return sai_status_t The status of the attribute retrieval operation.
          */
-        sai_status_t get_attr(_Out_ sai_attribute_t &attr) const override;
-
-        /**
-         * @brief Retrieves the value of a specific attribute of the SAI object. If the attribute is not found, log an error and 
-         *        return SAI_STATUS_FAILURE.
-         * 
-         * @param attr [out] A reference to the sai_attribute_t structure to store the attribute value.
-         * @return sai_status_t The status of the attribute retrieval operation.
-         */
-        sai_status_t get_mandatory_attr(_Out_ sai_attribute_t &attr) const override;
+        sai_status_t get_attr(_Inout_ sai_attribute_t &attr) const override;
 
     private:
         /**< The number of attributes associated with the SAI object. */
@@ -143,16 +148,7 @@ namespace saivpp
          * @param attr The attribute to be retrieved.
          * @return The status of the operation.
          */
-        sai_status_t get_attr(_Out_ sai_attribute_t &attr) const override;
-        
-        /**
-         * @brief Retrieves the value of a specific attribute of the SAI object. If the attribute is not found, log an error and 
-         *        return SAI_STATUS_FAILURE.
-         * 
-         * @param attr [out] A reference to the sai_attribute_t structure to store the attribute value.
-         * @return sai_status_t The status of the attribute retrieval operation.
-         */
-        sai_status_t get_mandatory_attr(_Out_ sai_attribute_t &attr) const override;
+        sai_status_t get_attr(_Inout_ sai_attribute_t &attr) const override;
 
         /**
          * @brief Retrieves the child objects of the specified type.
@@ -205,10 +201,44 @@ namespace saivpp
     private:
         /**
          * @brief A map of child objects based on their type and ID.
+         * child-type -> child-oid -> child-object
          */
         std::unordered_map<sai_object_type_t, std::unordered_map<std::string, std::shared_ptr<SaiObject>>> m_child_map;
     };
     
+    /**
+     * SaiModDBObject is used to represent a SAI object that has been modified. It has the list of modified attributes of 
+     * the SAI object passed in through set request and backed by original SAI object in DB. get_attribute will first read
+     * from the modified attribute list and if not found, it will read the object in database.
+     */
+    class SaiModDBObject : public SaiObject {
+    public:
+        SaiModDBObject(SwitchStateBase* switch_db, sai_object_type_t type, const std::string& id, 
+                       uint32_t attr_count, const sai_attribute_t *attr_list);
+
+        ~SaiModDBObject() = default;
+
+        sai_status_t get_attr(_Inout_ sai_attribute_t &attr) const override;
+
+        std::shared_ptr<SaiDBObject> get_db_obj() const { 
+            return m_sai_db_obj; 
+        }
+        
+        const std::unordered_map<std::string, std::shared_ptr<SaiObject>>* get_child_objs(sai_object_type_t child_type) const {
+            if (m_sai_db_obj) {
+                return m_sai_db_obj->get_child_objs(child_type);
+            } else {
+                return nullptr;
+            }
+        }
+    private:
+        /**< The number of modified attributes associated with the SAI object. */
+        uint32_t m_attr_count;
+        /**< An array of sai_attribute_t structures representing the modified attributes of the SAI object. */
+        const sai_attribute_t *m_attr_list;    
+        std::shared_ptr<SaiDBObject> m_sai_db_obj;
+    };
+
     /**
      * @brief The SaiObjectDB class represents a database for managed SAI objects. Only the SAI objects we are interested are
      * added to SaiObjectDB. See sai_child_relation_defs in SaiObjectDB.cpp for such objects.
@@ -233,21 +263,22 @@ namespace saivpp
         ~SaiObjectDB() = default;
 
         /**
-         * @brief Adds a new SAI object to the database.
+         * @brief Adds a new SAI object to the database or update an existing one if it already exists, 
+         *      which includes removing the child from the current parent and adding it to the new parent.
          * 
          * @param object_type The type of the SAI object.
          * @param id The ID of the SAI object.
-         * @param switch_id The ID of the switch to which the SAI object belongs.
          * @param attr_count The number of attributes in the attribute list.
          * @param attr_list An array of attributes for the SAI object.
+         * @param is_create A flag indicating whether the object is being created.
          * @return The status of the operation.
          */
-        sai_status_t add(
+        sai_status_t create_or_update(
                     _In_ sai_object_type_t object_type,
                     _In_ const std::string& id,
-                    _In_ sai_object_id_t switch_id,
                     _In_ uint32_t attr_count,
-                    _In_ const sai_attribute_t *attr_list);
+                    _In_ const sai_attribute_t *attr_list,
+                    _In_ bool is_create);
 
         /**
          * @brief Removes an existing SAI object from the database.
@@ -274,7 +305,27 @@ namespace saivpp
     private:
         // Pointer to the switch database
         SwitchStateBase* m_switch_db; 
-        // Map of SAI parent objects based on their object type and ID
+        /**
+         * @brief A map of SAI parent objects based on their type and ID.
+         *  parent-type -> parent-oid -> parent-object
+         * a parent object has a map of child objects based on their type and ID.
+         */ 
         std::unordered_map<sai_object_type_t, std::unordered_map<std::string, std::shared_ptr<SaiDBObject>>> m_sai_parent_objs; 
+        /**
+         * @brief Removes a child object from its parent in the SaiObjectDB for the give child-parent relationship definition.
+         *
+         * This function removes a child object from its parent in the SaiObjectDB. It takes the object type, the ID of the child object, 
+         * and the child-parent relationship definition as input parameters.
+         * The function retrieves the parent object IDs using the get_parent_oids() function and iterates over each parent ID.
+         * For each parent ID, it checks if the parent object exists in the SaiObjectDB. If the parent object is not found, a warning 
+         * message is logged and the function returns SAI_STATUS_SUCCESS.
+         * If the parent object is found, the function removes the child object from the parent object using the remove_child() 
+         * function and logs a debug message.
+         *
+         * @param object_type The type of the child object.
+         * @param id The ID of the child object.
+         * @param child_def The child-parent relationship definition.
+         */
+        void remove_child_from_parent(_In_ sai_object_type_t object_type, _In_ const std::string& id, _In_ const SaiChildRelation& child_def);
     };
 }
