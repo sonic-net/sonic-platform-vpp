a64540a88a3499b10a0cf63a7b6ff87b66ad0df2
Litmingc <37037390+litmingc@users.noreply.github.com>
2025-06-03 21:35:58 +0800

    Mount the host's hugepages to docker-syncd-vpp (#181)
    Signed-off-by: chenming_yewu@cmss.chinamobile.com
    
    Signed-off-by: chenming_yewu@cmss.chinamobile.com

--- rules/docker-syncd-vpp.mk
+++ rules/docker-syncd-vpp.mk
@@ -45,3 +45,4 @@ $(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /dev/hugepages:/dev/hugepages
