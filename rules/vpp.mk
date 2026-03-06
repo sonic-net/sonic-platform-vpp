1d288b7a1c75ad8d2e41765906c35dfb0f53e493
Yue Gao <yuega2@cisco.com>
2026-03-06 12:03:28 -0800

    Relocate vpp.mk to platform/vpp
    Signed-off-by: Yue Gao <yuega2@cisco.com>

---
+++ rules/vpp.mk
@@ -0,0 +1,59 @@
+# libvpp package
+
+VPP_VERSION_BASE = 2510
+VPP_VERSION = $(VPP_VERSION_BASE)-0.1
+VPP_VERSION_SONIC = $(VPP_VERSION)+b1sonic1
+VPP_SRC_PATH = platform/vpp/vppbld
+
+export VPP_VERSION_SONIC
+export VPP_SRC_PATH
+
+VPPINFRA = libvppinfra_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPPINFRA)_SRC_PATH = $(VPP_SRC_PATH)
+$(VPPINFRA)_DEPENDS += $(LIBNL3) $(LIBNL3_DEV) $(LIBNL_ROUTE3) $(LIBNL_ROUTE3_DEV)
+SONIC_MAKE_DEBS += $(VPPINFRA)
+
+VPP_MAIN = vpp_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPP_MAIN)_DEPENDS += $(VPPINFRA)
+$(VPP_MAIN)_RDEPENDS += $(VPPINFRA)
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPP_MAIN)))
+
+LIBNUMA := $(shell apt-get download --print-uris libnuma1 | cut -d " " -f2)
+$(LIBNUMA)_URL := $(shell apt-get download --print-uris libnuma1 | cut -d " " -f1)
+SONIC_ONLINE_DEBS += $(LIBNUMA)
+
+VPP_PLUGIN_CORE = vpp-plugin-core_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPP_PLUGIN_CORE)_DEPENDS += $(VPP_MAIN)
+$(VPP_PLUGIN_CORE)_RDEPENDS += $(VPP_MAIN)
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPP_PLUGIN_CORE)))
+
+VPP_PLUGIN_DPDK = vpp-plugin-dpdk_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPP_PLUGIN_DPDK)))
+$(VPP_PLUGIN_DPDK)_DEPENDS += $(LIBNUMA) $(VPP_MAIN)
+$(VPP_PLUGIN_DPDK)_RDEPENDS += $(LIBNUMA) $(VPP_MAIN)
+
+VPPINFRA_DEV = libvppinfra-dev_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPPINFRA_DEV)_DEPENDS += $(VPPINFRA)
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPPINFRA_DEV)))
+
+VPP_PLUGIN_DEV = vpp-plugin-devtools_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPP_PLUGIN_DEV)_DEPENDS += $(VPP_MAIN) $(VPPINFRA)
+$(VPP_PLUGIN_DEV)_RDEPENDS += $(VPP_MAIN) $(VPPINFRA)
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPP_PLUGIN_DEV)))
+
+VPP_DEV = vpp-dev_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(VPP_DEV)_DEPENDS += $(VPPINFRA_DEV)
+$(VPP_DEV)_RDEPENDS += $(VPPINFRA_DEV)
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPP_DEV)))
+
+VPPDBG = vpp-dbg_$(VPP_VERSION_SONIC)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(VPPINFRA),$(VPPDBG)))
+
+# Add VPP build/runtime dependencies to sairedis packages
+$(LIBSAIREDIS)_DEPENDS += $(VPP_MAIN) $(VPP_PLUGIN_CORE) $(VPP_PLUGIN_DPDK) \
+	$(VPP_PLUGIN_DEV) $(VPP_DEV) $(VPPINFRA_DEV) $(VPPDBG)
+$(LIBSAIVS)_RDEPENDS += $(VPP_MAIN) $(VPP_PLUGIN_CORE) $(VPP_PLUGIN_DPDK)
+
+VPP_VERSION_FILE = vpp_$(VPP_VERSION_SONIC)_commit
+
+export VPP_MAIN VPPINFRA VPP_PLUGIN_CORE VPP_PLUGIN_DPDK VPP_PLUGIN_DEV VPP_DEV VPPINFRA_DEV VPPDBG VPP_VERSION_FILE
