6311009337ad8c553956147c928c887ae1f72d6d
dypet <dypeters@cisco.com>
2026-04-27 17:54:09 -0400

    Move platform vpp to Trixie.
    Signed-off-by: dypet <dypeters@cisco.com>

--- rules/docker-gbsyncd-vpp.mk
+++ rules/docker-gbsyncd-vpp.mk
@@ -15,7 +15,7 @@
 # docker image for vpp gbsyncd
 
 DOCKER_GBSYNCD_PLATFORM_CODE = vpp
-include $(PLATFORM_PATH)/../template/docker-gbsyncd-bookworm.mk
+include $(PLATFORM_PATH)/../template/docker-gbsyncd-trixie.mk
 
 $(DOCKER_GBSYNCD_BASE)_DEPENDS += $(LIBSAIVS) \
                                 $(SYNCD_VS)
@@ -28,5 +28,3 @@ $(DOCKER_GBSYNCD_BASE)_DBG_DEPENDS += $(SYNCD_VS_DBG) \
 
 $(DOCKER_GBSYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_GBSYNCD_BASE)_PACKAGE_NAME = gbsyncd
-
-$(DOCKER_GBSYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
